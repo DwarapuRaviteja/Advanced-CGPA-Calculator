c2420662279522ec7a6358e18bf4cb7ee4532151
Dwarapu Ravi Teja <ravitejadwarapu9@gmail.com>
2026-07-10 13:53:08 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -166,3 +166,48 @@ function calculateAll(){
         errorDiv.classList.remove("hidden")
     })
 }
+
+async function downloadPDF() {
+
+    const { jsPDF } = window.jspdf;
+
+    const element = document.getElementById("pdfContent");
+
+    const canvas = await html2canvas(element,{
+        scale:2,
+        useCORS:true
+    });
+
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF("p","mm","a4");
+
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+
+    const pdfHeight = pdf.internal.pageSize.getHeight();
+
+    const imgWidth = pdfWidth;
+
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    let heightLeft = imgHeight;
+
+    let position = 0;
+
+    pdf.addImage(imgData,"PNG",0,position,imgWidth,imgHeight);
+
+    heightLeft -= pdfHeight;
+
+    while(heightLeft > 0){
+
+        position = heightLeft - imgHeight;
+
+        pdf.addPage();
+
+        pdf.addImage(imgData,"PNG",0,position,imgWidth,imgHeight);
+
+        heightLeft -= pdfHeight;
+    }
+
+    pdf.save("Advanced_CGPA_Report.pdf");
+}
